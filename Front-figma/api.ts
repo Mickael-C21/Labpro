b2c5aeb7f57402e1b9d5f00556608164489d6f46
Mickael Chrysolithe Tsoumbou <165265194+Mickael-C21@users.noreply.github.com>
2026-08-14 07:54:22 +0200

    Update API_URL environment variable name

--- Front-figma/api.ts
+++ Front-figma/api.ts
@@ -1,4 +1,4 @@
-﻿const API_URL = import.meta.env.VITE_API_BASE_URL ?? "http://127.0.0.1:8000";
+﻿const API_URL = import.meta.env.VITE_API_URL ?? "http://127.0.0.1:8000";
 
 type JsonValue = string | number | boolean | null | JsonObject | JsonValue[];
 interface JsonObject {
